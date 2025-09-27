a97733313afbbd4574ee27269e79574c0772493c
Vinaydwiveda <165122239+Vinaydwiveda@users.noreply.github.com>
2025-09-27 17:11:02 +0530

    Update api.js
    g

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: '/api', // use proxy for development
+  baseURL: 'https://smartclassroom-backend.onrender.com', // use proxy for development
 });
 
 export default API;
